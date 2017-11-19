b4d3db176621286ec3a95ab0ff6d52bb5a04b187
Michael <alias.micha@posteo.de>
2017-11-19 12:17:00 +0100

    Initial commit

---
+++ src/main/java/com/greenkaktus/tendence/TendenceApplication.java
@@ -0,0 +1,12 @@
+package com.greenkaktus.tendence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TendenceApplication.class, args);
+	}
+}
